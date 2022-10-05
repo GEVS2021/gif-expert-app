f756987d5fee9600badeaac8fe31836162388682
EduEstula1003 <edu.estula@ext.blaze.me>
2022-10-05 17:29:30 -0500

    80. GifGrid - Nuevo Componente

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import AddCategory from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
 
 
 const GifExpertApp = () => {
@@ -25,9 +26,12 @@ const GifExpertApp = () => {
 
             <ol>
                 { 
-                    categories.map(category => {
-                        return <li key={category}>{ category }</li>
-                    })
+                    categories.map(category => (
+                        <GifGrid
+                            key={category}
+                            category={category}
+                        />
+                    ))
                 }
             </ol>
         </>
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+
+const GifGrid = (props) => {
+
+
+
+    return (
+        <h3>{props.category}</h3>
+    )
+}
+
+export default GifGrid;
